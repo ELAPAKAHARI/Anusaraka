752f74523b0b520511814ce59e5c771077b9cfa0
roja <roja19p@gmail.com>
2011-10-11 15:35:11 +0530

    1. Added a rule in add_snt_mrkr.lex to handle sentence ending with double quotes. 2. Modified 'id-link_id_map_for_first_word' rule to handle sentences starting with double quotes in parserid_wordid_mapping.clp 3. Handled punctuations like '(', ')', ';', '=' in ALL_TRAN_PADA.c. 4. Handled '=' in f_range_wordarray_resarray.c. 5. Modified run_penn.sh and run_stanford.sh to handle 'americanize=false'. 6. Updated Stanford_parser_fails 7. Updated Log.

--- Parsers/stanford-parser/stanford-parser-2010-11-30/run_penn.sh
+++ Parsers/stanford-parser/stanford-parser-2010-11-30/run_penn.sh
@@ -2,5 +2,5 @@ export scriptdir=`dirname $0`
 
 #java  -mx900m -cp "$scriptdir/stanford-parser.jar:" edu.stanford.nlp.parser.lexparser.LexicalizedParser -sentences "newline" -outputFormat penn -outputFormatOptions "treeDependencies" $scriptdir/englishPCFG.ser.gz $*
 
-java  -mx900m -cp "$scriptdir/stanford-parser.jar:" edu.stanford.nlp.parser.lexparser.LexicalizedParser -sentences "newline" -outputFormat oneline -outputFormatOptions "treeDependencies" $scriptdir/englishPCFG.ser.gz $*
+java  -mx900m -cp "$scriptdir/stanford-parser.jar:" edu.stanford.nlp.parser.lexparser.LexicalizedParser -sentences "newline" -outputFormat oneline -outputFormatOptions "treeDependencies" -tokenizerOptions americanize=false $scriptdir/englishPCFG.ser.gz $*
 
--- Parsers/stanford-parser/stanford-parser-2010-11-30/run_stanford.sh
+++ Parsers/stanford-parser/stanford-parser-2010-11-30/run_stanford.sh
@@ -4,8 +4,7 @@ export scriptdir=`dirname $0`
 
 #java  -mx500m -cp "$scriptdir/stanford-parser.jar:" edu.stanford.nlp.parser.lexparser.LexicalizedParser -sentences "newline" -outputFormat wordsAndTags,typedDependencies -outputFormatOptions "basicDependencies, CCPropagatedDependencies, treeDependencies" $scriptdir/englishPCFG.ser.gz $*
 
-##presently running below one
-java  -mx900m -cp "$scriptdir/stanford-parser.jar:" edu.stanford.nlp.parser.lexparser.LexicalizedParser -retainTmpSubcategories -sentences "newline" -outputFormat wordsAndTags,typedDependencies -outputFormatOptions "basicDependencies, CCPropagatedDependencies, treeDependencies" $scriptdir/englishPCFG.ser.gz $*
+#java  -mx900m -cp "$scriptdir/stanford-parser.jar:" edu.stanford.nlp.parser.lexparser.LexicalizedParser -retainTmpSubcategories -sentences "newline" -outputFormat wordsAndTags,typedDependencies -outputFormatOptions "basicDependencies, CCPropagatedDependencies, treeDependencies" $scriptdir/englishPCFG.ser.gz $*
 
 #java  -mx900m -cp "$scriptdir/stanford-parser.jar:" edu.stanford.nlp.parser.lexparser.LexicalizedParser -retainTmpSubcategories -sentences "newline" -outputFormat wordsAndTags,typedDependencies -printPCFGkBest 4 -outputFormatOptions "basicDependencies, CCPropagatedDependencies, treeDependencies" $scriptdir/englishPCFG.ser.gz $*
 
@@ -15,3 +14,6 @@ java  -mx900m -cp "$scriptdir/stanford-parser.jar:" edu.stanford.nlp.parser.lexp
 #java  -mx500m -cp "$scriptdir/stanford-parser.jar:" edu.stanford.nlp.parser.lexparser.LexicalizedParser -sentences "newline" -outputFormat wordsAndTags,typedDependencies,penn -outputFormatOptions "lexicalize" $scriptdir/englishPCFG.ser.gz $*
 
 
+##presently running below one
+### americanize=false
+java  -mx900m -cp "$scriptdir/stanford-parser.jar:" edu.stanford.nlp.parser.lexparser.LexicalizedParser -retainTmpSubcategories -sentences "newline" -outputFormat wordsAndTags,typedDependencies -outputFormatOptions "basicDependencies, CCPropagatedDependencies, treeDependencies"  -tokenizerOptions americanize=false $scriptdir/englishPCFG.ser.gz $*
