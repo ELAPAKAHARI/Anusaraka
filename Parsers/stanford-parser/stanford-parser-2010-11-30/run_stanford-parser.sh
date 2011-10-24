363c6a5925a6482d919b76ccdd20040fa0bff86b
mahalaxmi <mahalaxmi@anu.(none)>
2011-10-24 14:43:34 +0530

    1. Modified existing rules in H_ordering_rules.clp (Modified ids to P-ids)     2. Modified run_modules_std.bat, sd_combining_apostrophe.clp, run_stanford-parser.sh , constituency_parse.l , constituency_parse.y and Anusaaraka_stanford.sh files (for handling POS and punctuations)     3. Updated Log file.

--- Parsers/stanford-parser/stanford-parser-2010-11-30/run_stanford-parser.sh
+++ Parsers/stanford-parser/stanford-parser-2010-11-30/run_stanford-parser.sh
@@ -1,8 +1,8 @@
 ./constituency_to_dependency.sh $2/tmp/$1_tmp/one_sentence_per_line.txt.std.penn 1>$2/tmp/$1_tmp/sd-relations_tmp.txt 
 #./tsurgeon.sh -treeFile $2/tmp/$1_tmp/one_sentence_per_line.txt.std.penn tsurgeon-grammar > $2/tmp/$1_tmp/one_sentence_per_line.txt.std.tsurgeon
 #sh run_lexicalize.sh $2/tmp/$1_tmp/one_sentence_per_line.txt.std.tsurgeon 1>$2/tmp/$1_tmp/sd-lexicalize_info_tmp.txt 2>/dev/null
-#sh run_lexicalize.sh $2/tmp/$1_tmp/one_sentence_per_line.txt.std.cons 1>$2/tmp/$1_tmp/sd-lexicalize_info_tmp.txt 2>/dev/null
-sh run_lexicalize.sh $2/tmp/$1_tmp/one_sentence_per_line.txt.std.penn 1>$2/tmp/$1_tmp/sd-lexicalize_info_tmp.txt 2>/dev/null
+sh run_lexicalize.sh $2/tmp/$1_tmp/one_sentence_per_line.txt.std.cons 1>$2/tmp/$1_tmp/sd-lexicalize_info_tmp.txt 2>/dev/null
+#sh run_lexicalize.sh $2/tmp/$1_tmp/one_sentence_per_line.txt.std.penn 1>$2/tmp/$1_tmp/sd-lexicalize_info_tmp.txt 2>/dev/null
 sed -n -e "H;\${g;s/\n\n/\n;~~~~~~~~~~\n/g;p}" <$2/tmp/$1_tmp/sd-lexicalize_info_tmp.txt > $2/tmp/$1_tmp/sd-lexicalize_info.txt
 python split-relations.py $2/tmp/$1_tmp/sd-relations_tmp.txt  $2/tmp/$1_tmp/sd-basic_relations_tmp.txt $2/tmp/$1_tmp/sd-propagation_relations_tmp.txt $2/tmp/$1_tmp/sd-tree_relations_tmp.txt 
 ./stnford_relations.out $2/tmp/$1_tmp/sd-tree_relation.txt rel_name-sids < $2/tmp/$1_tmp/sd-tree_relations_tmp.txt
