0967dd9f700f511d33aaa13bb32649ef516fd9cf
mahalaxmi <mahalaxmi@anu.(none)>
2011-12-22 15:16:26 +0530

    For running specific parse in stanford :: 	  1.Anusaaraka_stanford.sh file has be modified. 	  2.Added new files run_multiple_parse_penn.sh and preffered_parse.py .

---
+++ Parsers/stanford-parser/stanford-parser-2010-11-30/run_multiple_parse_penn.sh
@@ -0,0 +1,4 @@
+export scriptdir=`dirname $0`
+
+java  -mx900m -cp "$scriptdir/stanford-parser.jar:" edu.stanford.nlp.parser.lexparser.LexicalizedParser -sentences "newline" -outputFormat oneline -outputFormatOptions "treeDependencies" -printPCFGkBest 100 -tokenizerOptions americanize=false $scriptdir/englishPCFG.ser.gz $*
+
