07806c2034b1127b1f7bff001fe409f6f52a93ad
roja <roja19p@gmail.com>
2012-01-05 13:16:25 +0530

    1. Renamed run_penn.sh as run_penn-pcfg.sh and updated the same in Anusaaraka_stanford.sh. 2. Renamed run_stanford-gui.sh as run_stanford-gui-pcfg.sh. 3. Added Anusaaraka_stanford-Factored.sh and run_stanford-gui-factored.sh in bin folder. 4. Added run_penn-factored.sh inside stanford parser. 5. Updated roman_iscii file. 6. Updated README inside stanford-parser. 7. Updated Log.

--- Parsers/stanford-parser/stanford-parser-2010-11-30/run_penn-pcfg.sh
+++ Parsers/stanford-parser/stanford-parser-2010-11-30/run_penn-pcfg.sh
@@ -1,3 +1,5 @@
+## On Chaitanya Sir suggestion renamed run_penn.sh as run_penn-pcfg.sh (Roja 05-01-11)
+
 export scriptdir=`dirname $0`
 
 #java  -mx900m -cp "$scriptdir/stanford-parser.jar:" edu.stanford.nlp.parser.lexparser.LexicalizedParser -sentences "newline" -outputFormat penn -outputFormatOptions "treeDependencies" $scriptdir/englishPCFG.ser.gz $*
