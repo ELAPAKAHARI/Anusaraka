6656f1faeb77557d2925135bd5850d72ac4d4884
roja <roja19p@gmail.com>
2012-11-24 18:07:26 +0530

    1.Added test condition in 'NN_to_NNP' rule to avoid numbers converted into NNP category. Ex: 84kms 2.Added tokenizer option 'escapeForwardSlashAsterisk' to avoid escape character in front of slash & asterisk in following files ::  run_penn-factored.sh and run_multiple_parse_penn.sh 3.Updated replace symbols for html files in run_align_browser.sh 4.Updated entry in iit-bombay dictionary for goldendict. 5.Updated Log

--- Parsers/stanford-parser/stanford-parser-2010-11-30/run_multiple_parse_penn.sh
+++ Parsers/stanford-parser/stanford-parser-2010-11-30/run_multiple_parse_penn.sh
@@ -1,4 +1,4 @@
 export scriptdir=`dirname $0`
 
-java  -mx900m -cp "$scriptdir/stanford-parser.jar:" edu.stanford.nlp.parser.lexparser.LexicalizedParser -sentences "newline" -outputFormat oneline -outputFormatOptions "treeDependencies" -printPCFGkBest 25 -tokenizerOptions americanize=false $scriptdir/englishPCFG.ser.gz $*
+java  -mx900m -cp "$scriptdir/stanford-parser.jar:" edu.stanford.nlp.parser.lexparser.LexicalizedParser -sentences "newline" -outputFormat oneline -outputFormatOptions "treeDependencies" -printPCFGkBest 25 -tokenizerOptions "americanize=false, escapeForwardSlashAsterisk=false" $scriptdir/englishPCFG.ser.gz $*
 
--- Parsers/stanford-parser/stanford-parser-2010-11-30/run_penn-factored.sh
+++ Parsers/stanford-parser/stanford-parser-2010-11-30/run_penn-factored.sh
@@ -4,4 +4,4 @@
 export scriptdir=`dirname $0`
 
 
-java  -mx900m -cp "$scriptdir/stanford-parser.jar:" edu.stanford.nlp.parser.lexparser.LexicalizedParser -sentences "newline" -outputFormat oneline -outputFormatOptions "treeDependencies" -tokenizerOptions americanize=false $scriptdir/englishFactored.ser.gz $*
+java  -mx900m -cp "$scriptdir/stanford-parser.jar:" edu.stanford.nlp.parser.lexparser.LexicalizedParser -sentences "newline" -outputFormat oneline -outputFormatOptions "treeDependencies" -tokenizerOptions "americanize=false, escapeForwardSlashAsterisk=false" $scriptdir/englishFactored.ser.gz $*
